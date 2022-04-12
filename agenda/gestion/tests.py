7d0962715be347617fb8f9c59eb97472c98be6c3
danny cerda <danny.cerda@talma.com.pe>
2022-04-11 20:04:24 -0500

    eliminarCarpetas

--- agenda/gestion/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from rest_framework.test import APITestCase
-from unittest import TestCase
-
-class EtiquetasTestCase(APITestCase):
-    def test_crear_etiqueta_success(self):
-        self.assertEqual(1,1)
